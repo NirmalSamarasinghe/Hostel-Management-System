6685b2e27a855cc232b182be5b900ca8b67325ae
Nipun78 <nipunnirmal67@gmail.com>
2023-08-15 21:40:30 +0530

    add the login button

--- src/main/java/controller/HomePageFormController.java
+++ src/main/java/controller/HomePageFormController.java
@@ -1,4 +1,27 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HomePageFormController {
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    void btnStartOnAction(ActionEvent event) throws IOException {
+
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/view/login_page_form.fxml"));
+        Scene scene = new Scene(anchorPane);
+        Stage stage = (Stage) root.getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Login Form");
+        stage.centerOnScreen();
+    }
 }
