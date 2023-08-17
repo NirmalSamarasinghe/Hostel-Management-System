136a05d5c966419b4cd8b57e6d20f329c48b2fac
Nipun78 <nipunnirmal67@gmail.com>
2023-08-17 21:44:39 +0530

    create the dashboard  controller class & design

---
+++ src/main/java/controller/DashBordeFormController.java
@@ -0,0 +1,47 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashBordeFormController implements Initializable {
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    void btnDashboardOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnAddStudentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogOutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReservationOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRoomsOnAction(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
