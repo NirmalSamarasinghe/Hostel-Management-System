d0d3f251fea00521359a1610b54956817cdb7d6a
Nipun78 <nipunnirmal67@gmail.com>
2023-08-17 21:24:35 +0530

    update the login page & class

--- src/main/java/controller/LoginPageFormController.java
+++ src/main/java/controller/LoginPageFormController.java
@@ -1,6 +1,47 @@
 package controller;
 
-public class LoginPageFormController {
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class LoginPageFormController implements Initializable {
+    @FXML
+    public AnchorPane root;
+
+    @FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    private JFXPasswordField txtPassword;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/view/DashBorde_form.fxml"));
+        Scene scene = new Scene(anchorPane);
+        Stage stage = (Stage) root.getScene().getWindow();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.setTitle("DashBord");
+
+    }
+
+    @FXML
+    void btnNewHereOnAction(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
